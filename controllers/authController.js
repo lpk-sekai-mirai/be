0433a0857d7829fb05ef617c26fbf57ec93b2f5b
anqalah <evodark7@gmail.com>
2026-09-21 11:10:13 +0800

    jwt session update

--- controllers/authController.js
+++ controllers/authController.js
@@ -48,7 +48,7 @@ export const login = async (req, res) => {
     const token = jwt.sign(
       { id: user.id, role: user.role },
       process.env.JWT_SECRET,
-      { expiresIn: "15m" }
+      { expiresIn: "1d" }
     );
     res.json({
       token,
